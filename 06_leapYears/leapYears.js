1458bfbe1de1151e78e49b3aa3b72f49cdfca3bd
tod-drol <tod.dark13@gmail.com>
2024-07-23 14:36:24 -0700

    Add passing tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+/* checks for 29 days in the month of February of the year passed in the
+ argument, if true, then that year is a leap year */
+const leapYears = function (year) {
+  return new Date(year, 1, 29).getDate() === 29;
 };
 
 // Do not edit below this line
