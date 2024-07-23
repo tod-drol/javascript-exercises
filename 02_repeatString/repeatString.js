ae8a8e936c0989dfca31b1c6ed529e2bba62a365
tod-drol <tod.dark13@gmail.com>
2024-07-22 22:33:57 -0700

    Add passing tests from 1 to 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,14 @@
-const repeatString = function (string, n) {
-  let resultString = '';
-  for (let i = 1; i <= n; i++) {
-    if (string) {
+const repeatString = function (string, num) {
+  let resultString = "";
+  for (let i = 1; i <= num; i++) {
+    if (num) {
       resultString += string;
     } else {
-      return "Error";
+      break;
     }
+  }
+  if (num < 0) {
+    resultString += "ERROR";
   }
   return resultString;
 };
