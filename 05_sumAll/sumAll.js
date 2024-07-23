1198818d4ecab171c577ef74829c6714840ee1cf
tod-drol <tod.dark13@gmail.com>
2024-07-23 14:06:41 -0700

    Add passing tests 1 to 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  // create a loop that iterates from the start to the end and each iteration adds to result
+  let sum = 0;
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else if (start > end) {
+    for (let i = start; i >= end; i--) {
+      sum += i;
+    }
+  } else {
+    return sum = "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
