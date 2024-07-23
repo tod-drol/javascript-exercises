fe47c39ebcfcfbf12b22b79ad5bf4a6d3499cfc9
tod-drol <tod.dark13@gmail.com>
2024-07-22 21:57:09 -0700

    Add first passing test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, n) {
+  let resultString = '';
+  for (let i = 1; i <= n; i++) {
+    if (string) {
+      resultString += string;
+    } else {
+      return "Error";
+    }
+  }
+  return resultString;
 };
 
 // Do not edit below this line
